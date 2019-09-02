79bcfacfd19cd2ec637eac5624fc8ee228676772
Kartaviy-arteom <46100724+Kartaviy-arteom@users.noreply.github.com>
2019-09-02 11:05:49 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,30 +2,62 @@
 
 (function() {
 	var GAP = 124;
+  var IMAGE_GAP = 13;
+  var GALLERY_WRAPPER_WIDTH = 606;
 	var gallery = document.querySelector('.life__gallery');
 	var rightBtn = document.querySelector('.life__btn--right');
 	var leftBtn = document.querySelector('.life__btn--left');
 	var galleryPositionX = 0;
-	var measureElement = function (element) {
-	    return {
-	      x: element.offsetWidth,
-	      y: element.offsetHeight
-	    };
-  	};
-  	if (rightBtn) {
-  		rightBtn.addEventListener('click', function (evt) {
-  			evt.preventDefault();
-			galleryPositionX -= GAP;
-  			gallery.style.left = galleryPositionX + 'px';
-  		})
-  	};
+  var galleryLenght = gallery.children.length * GAP - IMAGE_GAP;
+  var mainGalleryImage = document.querySelector('.life__wrapper>img');
 
-  	if (leftBtn) {
-  		leftBtn.addEventListener('click', function (evt) {
-  			evt.preventDefault();
-			galleryPositionX += GAP;
-  			gallery.style.left = galleryPositionX + 'px';
-  		})
-  	};
+  var moveGallery = function (direction) {
+    if (direction === 'left') {
+      galleryPositionX -= GAP;
+    };
+    if (direction === 'right') {
+      galleryPositionX += GAP;
+    };
+    if (galleryPositionX > 0) {
+      galleryPositionX = 0;
+      leftBtn.classList.add('no-photo');
+    };
+
+    if (galleryLenght + galleryPositionX < GALLERY_WRAPPER_WIDTH) {
+      galleryPositionX = GALLERY_WRAPPER_WIDTH - galleryLenght;
+      rightBtn.classList.add('no-photo');
+    };
+
+    gallery.style.left = galleryPositionX + 'px';
+  };
+
+  var onRightBtnClick = function (evt) {
+    evt.preventDefault();
+    moveGallery('left');
+    if (leftBtn.classList.contains('no-photo')) {
+      leftBtn.classList.remove('no-photo');
+    };
+  };
+  var onLeftBtnClick = function (evt) {
+    evt.preventDefault();
+    moveGallery('right');
+    if (rightBtn.classList.contains('no-photo')) {
+      rightBtn.classList.remove('no-photo');
+    };
+  };
+
+	if (rightBtn) {
+		rightBtn.addEventListener('click', onRightBtnClick);
+  };
+
+	if (leftBtn) {
+		leftBtn.addEventListener('click', onLeftBtnClick);
+  };
+
+  gallery.addEventListener('click', function(evt) {
+    if(evt.target.tagName.toLowerCase() === 'img') {
+      mainGalleryImage.src = evt.target.dataset.bigImgSrc;
+    };
+  })
 
 })();
